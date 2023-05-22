393de12c235938c0b1d74fec3dfd123298f8cdf6
DeniArif <deniarifwibowo82@gmail.com>
2023-05-22 15:25:17 +0700

    comit

--- ParadigmaOPP_188/ParadigmaOPP_188.cpp
+++ ParadigmaOPP_188/ParadigmaOPP_188.cpp
@@ -8,5 +8,18 @@ private:
 	string nama;
 	float nilai;
 
-public
+public:
+	mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai) {
+		//Definisi
+	}
+
+	~mahasiswa() {
+		cout << "Id       = " << endl;
+		cout << "Nama     = " << endl;
+		cout << "Nilai    = " << endl;
+	}
 };
+
+int main() {
+	mahasiswa mhs(12, "Lia", 90.5);
+}
