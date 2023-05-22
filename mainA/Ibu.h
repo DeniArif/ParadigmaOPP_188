48e1717154a29becce0c0eb5bc8f792baabbdb7e
DeniArif <deniarifwibowo82@gmail.com>
2023-05-22 15:48:43 +0700

    comit

---
+++ mainA/Ibu.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef IBU_H
+#define IBU_H
+
+class ibu {
+	string nama;
+	vactor<anak*> daftar_anak;
+
+	ibu(string pNama) :nama(pNama) {
+		cout << "Ibu \"" << nama << "\" ada\n";
+	}
+	~ibu() {
+		cout << "ibu \"" << nama << "\" tidak ada\n";
+	}
+
+};
